9caa21df64823b5d5a8e599c22aeaeb4734ccdcc
bymoen <alexb@innotechcollege.com>
2023-03-14 18:44:48 -0600

    Added some variables and console logs

---
+++ js/app.js
@@ -0,0 +1,9 @@
+let item_one_name = "banana";
+let item_one_price = 0.35;
+let item_two_name = "strawberry";
+let item_two_price = 0.25;
+
+console.log(item_one_name);
+console.log(item_one_price);
+console.log(item_two_name);
+console.log(item_two_price);
