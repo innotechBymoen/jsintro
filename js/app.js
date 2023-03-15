80d831b542473ababfda09f5bec520728f699b27
bymoen <alexb@innotechcollege.com>
2023-03-14 20:47:33 -0600

    Added more conditionals with boolean operations

--- js/app.js
+++ js/app.js
@@ -8,10 +8,30 @@ console.log(item_one_price);
 console.log(item_two_name);
 console.log(item_two_price);
 
-let user_points = 74;
+let user_points = 45;
+let is_member = true;
+let user_name = "Alex";
 
 if(user_points >= 75){
     console.log("You did it! You have so many points!");
+} else if(user_points >= 50) {
+    console.log("Keep going, almost at 75!");
 } else {
     console.log("Keep going, get more points!");
 }
+
+
+if(is_member === true && user_points >= 90){
+    console.log("You are a talented member!");
+}
+
+if(is_member === false || user_points < 10) {
+    console.log("Sorry, you are either not a member, or a very untalented member. You must go.");
+}
+
+if(user_name === "Alex"){
+    console.log("You are a very special user.");
+} else {
+    console.log("You are a normal user.");
+}
+
